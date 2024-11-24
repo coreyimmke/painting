14fc70e8fbe05ff1121e82ac7f7b878c84e90248
Corey Immke <cimmke2@gmail.com>
2024-11-23 22:16:08 -0600

    Add episode detail page

--- episodes/urls.py
+++ episodes/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("episodes/", views.EpisodesView.as_view(), name="episodes"),
+    path("episodes/", views.EpisodesView.as_view(), name="episode_list"),
+    path(
+        "season/<int:season>/episode/<int:episode>/",
+        views.EpisodeDetailView.as_view(),
+        name="episode_detail",
+    ),
 ]
--- episodes/views.py
+++ episodes/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 from .models import Episode
 
@@ -7,3 +8,15 @@ class EpisodesView(generic.ListView):
     model = Episode
     template_name = "episodes/episode_list.html"
     paginate_by = 13  # 13 entries per page which aligns with num episodes per season
+
+
+class EpisodeDetailView(generic.DetailView):
+    model = Episode
+    template_name = "episodes/episode_detail.html"
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(
+            Episode,
+            season_number=self.kwargs.get("season"),
+            episode_number=self.kwargs.get("episode"),
+        )
